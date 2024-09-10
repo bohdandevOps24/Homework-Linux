aecb749a8f9187da4ec841cb8e62a71fac3f3910
Ubuntu <ubuntu@ip-172-31-17-7.ec2.internal>
2024-09-10 02:15:52 +0000

    ubuntu httpd install

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+sudo apt update
 sudo apt install apache2 \
                  ghostscript \
                  libapache2-mod-php \
@@ -15,7 +18,7 @@ sudo apt install apache2 \
                  php-xml \
                  php-zip -y
 sudo wget https://wordpress.org/latest.tar.gz
-tar -zxf latest.tar.gz
-sudo cp -r wordpress/* /var/www/html
+tar zxf latest.tar.gz
+sudo cp -rf wordpress/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html
 sudo systemctl restart apache2
